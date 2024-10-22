4c3efdd6682029e3a5b843185e71fa4e7207e8de
diegdar <diegdarch@hotmail.com>
2024-10-22 14:46:20 +0200

    feat: :sparkles: create methods: store and update and their form request

--- backendCrackathoners/app/Http/Controllers/StreetMarketController.php
+++ backendCrackathoners/app/Http/Controllers/StreetMarketController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStreetMarketRequest;
+use App\Http\Requests\UpdateStreetMarketRequest;
 use App\Models\StreetMarket;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -20,14 +22,16 @@ class StreetMarketController extends Controller
         //
     }
 
-    public function store(Request $request)
+    public function store(StoreStreetMarketRequest $request): JsonResponse
     {
-        //
+        $data = $request->validated();
+        $streetMarket = StreetMarket::create($data);
+        return response()->json("message: Street Market created successfully", 200);
     }
 
     public function show(StreetMarket $streetMarket)
     {
-        //
+        return response()->json($streetMarket);
     }
 
     public function edit(StreetMarket $streetMarket)
@@ -35,13 +39,17 @@ class StreetMarketController extends Controller
         //
     }
 
-    public function update(Request $request, StreetMarket $streetMarket)
+    public function update(UpdateStreetMarketRequest $request, StreetMarket $streetMarket): JsonResponse
     {
-        //
+        $data = $request->validated();
+        $streetMarket->update($data);
+        return response()->json("message: Street Market updated successfully", 200);
     }
 
-    public function destroy(StreetMarket $streetMarket)
+    public function destroy(StreetMarket $streetMarket) : JsonResponse
     {
-        //
+        $streetMarket->delete();
+        return response()->json(null, 204);
+
     }
 }
