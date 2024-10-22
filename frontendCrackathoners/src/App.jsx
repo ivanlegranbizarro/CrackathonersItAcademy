176448aae00766a0c1c7759497bac4543275e942
BCN-Asumiko <yakoshko@gmail.com>
2024-10-22 15:27:28 +0200

    feat: navbar is done

--- frontendCrackathoners/src/App.jsx
+++ frontendCrackathoners/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/navbar/Navbar'
 
 
 function App() {
 
   return (
     <>
+    <Navbar/>
       <h1>Hello World!</h1>
     </>
   )
