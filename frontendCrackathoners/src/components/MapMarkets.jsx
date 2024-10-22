2f865f1edd37395cb399b89d4da1c6cf698d2aac
Khrystsina  Kozak <36383143+Tinunsky@users.noreply.github.com>
2024-10-22 17:14:02 +0200

    style: media queries for mep

--- frontendCrackathoners/src/components/MapMarkets.jsx
+++ frontendCrackathoners/src/components/MapMarkets.jsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import styled from 'styled-components';
 
 
 const mercatsData = [
@@ -10,7 +11,7 @@ const mercatsData = [
     coord_lon: "2.149626569377951",
     type: "mercat",
     district: "Sant Gervasi",
-    adress: "Verdaguer, 12"
+    adress: "Verdaguer, 12",
   },
   {
     id: 2,
@@ -19,16 +20,16 @@ const mercatsData = [
     coord_lon: "2.1781153021938913",
     type: "mercat",
     district: "Sant Gervasi",
-    adress: "Verdaguer, 12"
+    adress: "Verdaguer, 12",
   },
   {
     id: 3,
     name: "Feria 3",
-    coord_lat: "41.382326716787425", 
+    coord_lat: "41.382326716787425",
     coord_lon: "2.173576130337292",
     type: "mercat",
     district: "Sant Gervasi",
-    adress: "Verdaguer, 12"
+    adress: "Verdaguer, 12",
   },
   {
     id: 4,
@@ -37,15 +38,28 @@ const mercatsData = [
     coord_lon: "2.122383312823837",
     type: "mercat",
     district: "Sant Gervasi",
-    adress: "Verdaguer, 12"
+    adress: "Verdaguer, 12",
   },
 ];
 
-  export const MapMarkets = () => {
-    const [coordinates] = useState([41.3919, 2.1649]);
+const MapContainerStyled = styled.div`
+  width: 85%;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 600px) {
+    width: 85%;
+  }
+
+  @media only screen and (min-width: 601px) {
+    width: 85%;
+  }
+`;
+
+export const MapMarkets = () => {
+  const [coordinates] = useState([41.3919, 2.1649]);
 
   return (
-    <div style={{ margin: "50px" }}>
+    <MapContainerStyled>
       <MapContainer
         center={coordinates}
         zoom={13}
@@ -55,7 +69,7 @@ const mercatsData = [
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
-          {mercatsData.map((marker) => (
+        {mercatsData.map((marker) => (
           <Marker
             key={marker.id}
             position={[Number(marker.coord_lat), Number(marker.coord_lon)]}
@@ -64,12 +78,11 @@ const mercatsData = [
               <p>{marker.type}</p>
               <p>{marker.name}</p>
               <p>{marker.district}</p>
-              <p>{marker.adress}</p>
-t
+              <p>{marker.adress}</p>t
             </Popup>
           </Marker>
         ))}
       </MapContainer>
-    </div>
+    </MapContainerStyled>
   );
 };
