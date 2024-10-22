e19d18fb7a1ebf21bf7a8a8cbd8b2a4929eade9d
Khrystsina  Kozak <36383143+Tinunsky@users.noreply.github.com>
2024-10-22 14:36:27 +0200

    MapLocal wip

--- frontendCrackathoners/src/components/MapLocal.jsx
+++ frontendCrackathoners/src/components/MapLocal.jsx
@@ -1,13 +1,30 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-export const MapLocal = () => {
-  const [coordinatesX, setCoordinatesX] = useState (40.416775)
-  const [coordinatesY, setCoordinatesY] = useState (-3.703790)
-  const coordinates = [coordinatesX, coordinatesY]
-  
+const feriasData = [
+  { id: 1, name: "Feria de Abril", coordinates: [37.3891, -5.9934] }, // Sevilla
+  { id: 2, name: "Feria de San Isidro", coordinates: [40.4170, -3.7038] }, // Madrid
+  { id: 3, name: "Feria de Málaga", coordinates: [36.7213, -4.4214] }, // Málaga
+  { id: 4, name: "Feria de Valencia", coordinates: [39.4699, -0.3763] }, // Valencia
+];
+
+
+export const MapLocal = ({selectedFeria}) => {
+  const [coordinatesX, setCoordinatesX] = useState(37.3891);
+  const [coordinatesY, setCoordinatesY] = useState(-5.9934);
+  const coordinates = [coordinatesX, coordinatesY];
+
+
+  useEffect(() => {
+    if (selectedFeria) {
+      setCoordinatesX(coordinatesX)
+      setCoordinatesY(coordinatesY)
+    }
+  }, [selectedFeria]);
+
+
   return (
-    <div style={{margin: "50px"}}>
+    <div style={{ margin: "50px" }}>
       <MapContainer
         center={coordinates}
         zoom={13}
