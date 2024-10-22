afe1a8d72aec1d64b47d235f03ed585b635e7ea8
BCN-Asumiko <yakoshko@gmail.com>
2024-10-22 16:59:42 +0200

    style: change map style, add more key-value pair to mock data

--- frontendCrackathoners/src/components/MapMarkets.jsx
+++ frontendCrackathoners/src/components/MapMarkets.jsx
@@ -8,24 +8,36 @@ const mercatsData = [
     name: "Feria de Abril",
     coord_lat: "41.35329883597558",
     coord_lon: "2.149626569377951",
+    type: "mercat",
+    district: "Sant Gervasi",
+    adress: "Verdaguer, 12"
   },
   {
     id: 2,
     name: "Feria de San Isidro",
     coord_lat: "41.3746514376551",
     coord_lon: "2.1781153021938913",
+    type: "mercat",
+    district: "Sant Gervasi",
+    adress: "Verdaguer, 12"
   },
   {
     id: 3,
     name: "Feria 3",
     coord_lat: "41.382326716787425", 
-    coord_lon: "2.173576130337292"
+    coord_lon: "2.173576130337292",
+    type: "mercat",
+    district: "Sant Gervasi",
+    adress: "Verdaguer, 12"
   },
   {
     id: 4,
     name: "Feria 4",
     coord_lat: "41.40014532869134",
-    coord_lon: "2.122383312823837"
+    coord_lon: "2.122383312823837",
+    type: "mercat",
+    district: "Sant Gervasi",
+    adress: "Verdaguer, 12"
   },
 ];
 
@@ -40,7 +52,7 @@ const mercatsData = [
         style={{ height: "100vh", width: "100%" }}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
           {mercatsData.map((marker) => (
@@ -48,7 +60,13 @@ const mercatsData = [
             key={marker.id}
             position={[Number(marker.coord_lat), Number(marker.coord_lon)]}
           >
-            <Popup>{marker.name}</Popup>
+            <Popup>
+              <p>{marker.type}</p>
+              <p>{marker.name}</p>
+              <p>{marker.district}</p>
+              <p>{marker.adress}</p>
+t
+            </Popup>
           </Marker>
         ))}
       </MapContainer>
