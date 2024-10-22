ce1ab700db516385daba7e9fb4e484d944fb1431
BCN-Asumiko <yakoshko@gmail.com>
2024-10-22 12:12:57 +0200

    initial commit with configuration for front end

---
+++ frontendCrackathoners/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styled from "styled-components";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
