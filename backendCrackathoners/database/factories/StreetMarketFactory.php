11aa46c08baaa04e63461311cea620c3d4445835
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-22 14:53:58 +0200

    first setup

--- backendCrackathoners/database/factories/StreetMarketFactory.php
+++ backendCrackathoners/database/factories/StreetMarketFactory.php
@@ -17,7 +17,19 @@ class StreetMarketFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'name' => fake()->name(),
+            'date_creation' => fake()->date(),
+            'address' => fake()->address(),
+            'address_number' => fake()->buildingNumber(),
+            'neighborhood' => fake()->city(),
+            'district' => fake()->city(),
+            'zip_code' => fake()->postcode(),
+            'town' => fake()->city(),
+            'coord_x' => fake()->latitude(),
+            'coord_y' => fake()->longitude(),
+            'phone' => fake()->phoneNumber(),
+            'schedule' => fake()->url(),
+            'type' => fake()->randomElement(['market', 'fair']),
         ];
     }
 }
--- backendCrackathoners/routes/api.php
+++ backendCrackathoners/routes/api.php
@@ -3,4 +3,4 @@
 use App\Models\StreetMarket;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('streetmakert', StreetMarket::class)->only(['index', 'show']);
+Route::apiResource('streetmarket', StreetMarket::class)->only(['index', 'show']);
